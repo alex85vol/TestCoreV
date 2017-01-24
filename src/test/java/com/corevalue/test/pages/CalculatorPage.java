c0142f055c8eb739669fdfd6239cea1b425ac89a
alex85vol <alex85vol@gmail.com>
2017-01-24 20:08:04 +0200

    new code

--- src/test/java/com/corevalue/test/pages/CalculatorPage.java
+++ src/test/java/com/corevalue/test/pages/CalculatorPage.java
@@ -1,7 +1,38 @@
 package com.corevalue.test.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 /**
  * Created by ovo on 24.01.2017.
  */
 public class CalculatorPage extends MainPage {
+
+    //region webelements field
+    private WebElement purchasePriceSlider;
+    private WebElement purchasePriceField;
+    private WebElement purchasePriceIncreaseButton;
+    private WebElement downPaymentIncreaseButton;
+    private WebElement downPaymentField;
+    private WebElement amortizationField;
+    private WebElement amortizationValue;
+    private WebElement paymentFrequency;
+    private WebElement interestRateField;
+
+
+    //endregion
+
+    public CalculatorPage(WebDriver driver) {
+        super(driver);
+        this.purchasePriceSlider = driver
+                .findElement(By.xpath(("//div[@class='slider-handle min-slider-handle custom'])[1]")));
+        this.purchasePriceField = driver.findElement(By.xpath("//input[@id='PrixPropriete']"));
+        this.purchasePriceIncreaseButton = driver
+                .findElement(By.xpath("//button[@class='PrixProprietePlus']"));
+        this.downPaymentIncreaseButton = driver.findElement(By.xpath("//button[@id='MiseDeFondPlus']"));
+        this.downPaymentField =driver.findElement(By.xpath("//input[@id='MiseDeFond']"));
+
+
+    }
 }
--- src/test/java/com/corevalue/test/pages/MortgagePage.java
+++ src/test/java/com/corevalue/test/pages/MortgagePage.java
@@ -1,7 +1,12 @@
 package com.corevalue.test.pages;
 
+import org.openqa.selenium.WebDriver;
+
 /**
  * Created by ovo on 24.01.2017.
  */
 public class MortgagePage extends MainPage {
+    protected MortgagePage(WebDriver driver) {
+        super(driver);
+    }
 }
