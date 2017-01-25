e1e9b914eb91cf0c4c8f80dec18b41927465b82c
alex85vol <alex85vol@gmail.com>
2017-01-25 14:25:51 +0200

    addde data for User

--- src/test/java/com/corevalue/test/data/UserRepository.java
+++ src/test/java/com/corevalue/test/data/UserRepository.java
@@ -3,5 +3,18 @@ package com.corevalue.test.data;
 /**
  * Created by ovo on 25.01.2017.
  */
-public class UserRepository {
+public final class UserRepository {
+
+    private UserRepository(){}
+
+    public static User getStandardUser() {
+        return User.get()
+                .setPurchasePrice(500000)
+                .setDownPayment(50000)
+                .setAmortization(15)
+                .setPaymentFrequency("weekly")
+                .setInterestRate(5.00)
+                .setCalculatedPayments(836.75)
+                .build();
+    }
 }
